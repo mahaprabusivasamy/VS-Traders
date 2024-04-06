a15567bfb8f21e24cb3613c2d3bacfe726e49fb7
MAHAPRABU S <115365595+mahaprabusivasamy@users.noreply.github.com>
2024-04-06 12:34:23 +0530

    Update ImageSlider.jsx

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -19,9 +19,9 @@ export default function ImageSlider() {
   }, [currentImageIndex]); // Ensure to include only currentImageIndex as dependency
 
   const images = [
-    slide,
-    slide,
-    slide
+     'https://img.freepik.com/free-photo/raw-meats-with-spices-utensils-rustic-cutting-board-dark-wood-top-view-ai-generative_123827-23528.jpg?w=1060&t=st=1712386871~exp=1712387471~hmac=af78deb997eba6db587539cbec13434995d819f1697783046a8101285a460919',
+   'https://img.freepik.com/free-photo/close-up-japanese-street-food_23-2149287811.jpg?w=1060&t=st=1712386948~exp=1712387548~hmac=4fcf9efade6bb5358f07aa39fc1092e168683e4df87aa59e46cfb60d1b99b9a1',
+'https://img.freepik.com/free-photo/top-view-meat-with-garlic-herbs_23-2148611056.jpg?w=1060&t=st=1712387004~exp=1712387604~hmac=b1ddc07ba3d5e1e4b8462026c9ce11e98f6f0e9a160af9335941d0577caa98cb'
   ];
 
   return (
